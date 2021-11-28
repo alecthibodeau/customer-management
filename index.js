ec4c148df5498db2a03ac3d3dde9e34c6c041ca7
alecthibodeau <alecthibodeau@gmail.com>
2021-11-28 13:17:48 -0500

    Generate names for directories

--- index.js
+++ index.js
@@ -6,27 +6,62 @@ const csv = require('csvtojson');
 const fs = require('fs');
 const path = require('path');
 
+const currentItem = 'LC22';
+const currentYear = 2021;
+let orderBlockDate = '';
+let dailyOrderPosition = 0;
+let namesOfDirectories = [];
+
+const leadingZeroAddition = (number) => {
+  for (let i = 0; i < 4 - number.toString().length; i++) {
+    number = '0' + number;
+  }
+  return number;
+}
+
 // Invoking csv returns a promise
 csv()
   .fromFile(`./${csvFile}`)
   .then((json) => {
     json.forEach(record => {
-      console.log(record['First Name']);
-      console.log(record['ZIP Code']);
+      let quantity = 1;
+      let quantityNotation = '';
+      let other = '';
+      if (record['Quantity'] > 1) {
+        quantity = record['Quantity'];
+        quantityNotation = `x${quantity}`;
+      }
+      if (record['Other Items']) {
+        other = `+${record['Other Items']}`;
+      }
+      const datePrefix = record['Date']
+        .replace('/', '')
+        .replace(`/${currentYear.toString()
+        .substring(2,4)}`, '')+`-${currentYear}-`;
+      const orderMonthAndDay = datePrefix.substring(0,4);
+      if (orderMonthAndDay !== orderBlockDate) {
+        dailyOrderPosition = 0;
+        orderBlockDate = orderMonthAndDay;
+      };
+      dailyOrderPosition++;
+      const dailyOrderIndex = leadingZeroAddition(dailyOrderPosition);
+      const basicOrderInfo = `${datePrefix}${dailyOrderIndex}${currentItem}${quantityNotation}`;
+      const directoryNameText = `${basicOrderInfo}${other} ${record.Customer}`;
+      namesOfDirectories.push(directoryNameText);
     })
   }
-);
-
-const customersArray = ['a', 'b', 'c']
-
-for (let i = 0; i < customersArray.length; i++) {
-  fs.mkdir(path.join(__dirname, customersArray[i]), (err) => {
-    if (err) {
-        if (err.errno === -17 && err.code === 'EEXIST') {
-          return console.error(`'${customersArray[i]}' directory already exists.`);
+)
+  .then(() => {
+    for (let i = 0; i < namesOfDirectories.length; i++) {
+      fs.mkdir(path.join(__dirname, namesOfDirectories[i]), (err) => {
+        if (err) {
+          if (err.errno === -17 && err.code === 'EEXIST') {
+            return console.error(`'${namesOfDirectories[i]}' directory already exists.`);
+          }
+          return console.error(err);
         }
-        return console.error(err);
-    }
-    console.log(`'${customersArray[i]}' directory successfully created.`);
-  });
-};
+        console.log(`'${namesOfDirectories[i]}' directory successfully created.`);
+      });
+    };
+  }
+);
